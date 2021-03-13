4a191dab1572fa0c11b5158227c4c3ff70c79931
Mueez Abdur-Rahman <mueez.abdurrahman@gmail.com>
2021-03-13 20:57:27 +0000

    ALARM-1 Testing Jira Integration

--- app/src/main/java/com/example/adhocalarm/MainActivity.kt
+++ app/src/main/java/com/example/adhocalarm/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.adhocalarm
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() { 
+class MainActivity : AppCompatActivity() { // Testing Jira integration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
