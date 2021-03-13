6b13eec206d62238d4afd389ba925c0701bc6c93
Mueez Abdur-Rahman <mueez.abdurrahman@gmail.com>
2021-03-13 20:03:41 +0000

    This will fail

--- app/src/main/java/com/example/adhocalarm/MainActivity.kt
+++ app/src/main/java/com/example/adhocalarm/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.adhocalarm
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() { // Hello this is my first proper commit.
+class MainActivity : AppCompatActivity() { 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
